93781dd2a2bb9036978b3dff13a6eff0d31f98eb
Nishant Vellanki <nishantvellanki@gmail.com>
2026-01-14 14:46:00 -0500

    Working on changing profile theme, fixed tab layout

--- app/(tabs)/playball.tsx
+++ app/(tabs)/playball.tsx
@@ -3,8 +3,5 @@ import { Text } from "react-native";
 export default function playball(){
     return(
         <Text>Play Ball</Text>
-        //Team management, Lineups, stats
-        //Could switch lineups to different tab, put team
-        //management and stats together
     )
 }
